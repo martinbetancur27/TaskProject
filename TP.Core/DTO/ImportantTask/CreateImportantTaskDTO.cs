f0962a73e7a45a1e4ec3336dbb01a2bbae7b72b6
Martin Betancur <martinbetancur27@gmail.com>
2022-11-14 18:53:48 -0500

    Add data annotations in DTOs and add form validator in Blazor pages

--- TP.Core/DTO/ImportantTask/CreateImportantTaskDTO.cs
+++ TP.Core/DTO/ImportantTask/CreateImportantTaskDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace TP.Core.DTO.ImportantTask
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP.Core.DTO.ImportantTask
 {
     public class CreateImportantTaskDTO
     {
+        [Required]
+        [MaxLength(60, ErrorMessage= "Sorry! The task must be a maximium of 60 characters")]
         public string Description { get; set; }
 
+        [Required]
         public DateTime EndDate { get; set; } = DateTime.Now;
     }
 }
--- TP.Core/DTO/UrgentTask/CreateUrgentTaskDTO.cs
+++ TP.Core/DTO/UrgentTask/CreateUrgentTaskDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace TP.Core.DTO.UrgentTask
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP.Core.DTO.UrgentTask
 {
     public class CreateUrgentTaskDTO
     {
+        [Required]
+        [MaxLength(60, ErrorMessage = "Sorry! The task must be a maximium of 60 characters")]
         public string Description { get; set; }
 
+        [Required]
         public DateTime EndDate { get; set; } = DateTime.Now;
     }
 }
